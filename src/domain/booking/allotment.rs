e88c3a48554dc01a69903e1e80b3aeb46886076e
thanhtan541 <thanhtan004100@gmail.com>
2023-12-17 18:47:57 +0700

    feat: add datetime

--- src/domain/booking/allotment.rs
+++ src/domain/booking/allotment.rs
@@ -1,3 +1,5 @@
+use chrono::prelude::*;
+
 pub struct Room {
     pub reference: String,
 }
@@ -12,12 +14,12 @@ impl Room {
 
 pub struct RoomEvent {
     pub reference: String,
-    pub start_date: String,
-    pub end_date: String,
+    pub start_date: DateTime<Utc>,
+    pub end_date: DateTime<Utc>,
 }
 
 impl RoomEvent {
-    pub fn new(reference: String, start_date: String, end_date: String) -> Self {
+    pub fn new(reference: String, start_date: DateTime<Utc>, end_date: DateTime<Utc>) -> Self {
         Self {
             reference,
             start_date,
@@ -44,18 +46,18 @@ mod test {
         let mut rooms_events = Vec::new();
         rooms_events.push(RoomEvent::new(
             "1".to_string(),
-            "2023-12-17".to_string(),
-            "2023-12-18".to_string(),
+            Utc.with_ymd_and_hms(2023, 12, 17, 0, 0, 0).unwrap(),
+            Utc.with_ymd_and_hms(2023, 12, 18, 0, 0, 0).unwrap(),
         ));
         rooms_events.push(RoomEvent::new(
             "1".to_string(),
-            "2023-12-17".to_string(),
-            "2023-12-18".to_string(),
+            Utc.with_ymd_and_hms(2023, 12, 17, 0, 0, 0).unwrap(),
+            Utc.with_ymd_and_hms(2023, 12, 18, 0, 0, 0).unwrap(),
         ));
         rooms_events.push(RoomEvent::new(
             "1".to_string(),
-            "2023-12-17".to_string(),
-            "2023-12-18".to_string(),
+            Utc.with_ymd_and_hms(2023, 12, 17, 0, 0, 0).unwrap(),
+            Utc.with_ymd_and_hms(2023, 12, 18, 0, 0, 0).unwrap(),
         ));
 
         assert_eq!(rooms_events.len(), 3);
