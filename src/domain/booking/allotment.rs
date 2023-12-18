61be28f767fa0b95d0fa23a98b29586156234bfc
thanhtan541 <thanhtan004100@gmail.com>
2023-12-18 23:27:49 +0700

    feat: add simple test

--- src/domain/booking/allotment.rs
+++ src/domain/booking/allotment.rs
@@ -14,14 +14,16 @@ impl Room {
 
 pub struct RoomEvent {
     pub reference: String,
+    pub room_id: i32,
     pub start_date: DateTime<Utc>,
     pub end_date: DateTime<Utc>,
 }
 
 impl RoomEvent {
-    pub fn new(reference: String, start_date: DateTime<Utc>, end_date: DateTime<Utc>) -> Self {
+    pub fn new(reference: String, room_id: i32, start_date: DateTime<Utc>, end_date: DateTime<Utc>) -> Self {
         Self {
             reference,
+            room_id,
             start_date,
             end_date,
         }
@@ -31,10 +33,10 @@ impl RoomEvent {
 
 #[cfg(test)]
 mod test {
-    use crate::domain::booking::allotment::*;
+    use super::*;
 
     #[test]
-    fn create_rooms() {
+    fn find_available_rooms() {
         // create rooms
         let mut rooms = Vec::new();
         rooms.push(Room::new("1".to_string()));
@@ -46,21 +48,31 @@ mod test {
         let mut rooms_events = Vec::new();
         rooms_events.push(RoomEvent::new(
             "1".to_string(),
+            1,
             Utc.with_ymd_and_hms(2023, 12, 17, 0, 0, 0).unwrap(),
-            Utc.with_ymd_and_hms(2023, 12, 18, 0, 0, 0).unwrap(),
-        ));
-        rooms_events.push(RoomEvent::new(
-            "1".to_string(),
-            Utc.with_ymd_and_hms(2023, 12, 17, 0, 0, 0).unwrap(),
-            Utc.with_ymd_and_hms(2023, 12, 18, 0, 0, 0).unwrap(),
-        ));
-        rooms_events.push(RoomEvent::new(
-            "1".to_string(),
-            Utc.with_ymd_and_hms(2023, 12, 17, 0, 0, 0).unwrap(),
-            Utc.with_ymd_and_hms(2023, 12, 18, 0, 0, 0).unwrap(),
+            Utc.with_ymd_and_hms(2023, 12, 20, 0, 0, 0).unwrap(),
         ));
 
-        assert_eq!(rooms_events.len(), 3);
+        assert_eq!(rooms_events.len(), 1);
+
+        let checkin_date = Utc.with_ymd_and_hms(2023, 12, 17, 0, 0, 0).unwrap();
+        let checkout_date = Utc.with_ymd_and_hms(2023, 12, 20, 0, 0, 0).unwrap();
+
+        // Expect available rooms are [2,3], since room 1 has event 
+        let mut occupied_rooms: Vec<i32> = Vec::new();
+        for room_event in rooms_events {
+            if room_event.start_date > checkin_date || room_event.start_date < checkout_date {
+                occupied_rooms.push(room_event.room_id);
+                continue;
+            }
+
+            if room_event.end_date > checkin_date || room_event.end_date < checkout_date {
+                occupied_rooms.push(room_event.room_id);
+                continue;
+            }
+        }
+
+        assert_eq!(occupied_rooms, [1]);
     }
 }
 
