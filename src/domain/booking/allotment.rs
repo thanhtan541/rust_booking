e6248dd82faee771908e2861ed149feea58963ea
thanhtan541 <thanhtan004100@gmail.com>
2023-12-20 10:15:26 +0700

    feat: refactor code

--- src/domain/booking/allotment.rs
+++ src/domain/booking/allotment.rs
@@ -30,18 +30,42 @@ impl RoomEvent {
     }
 }
 
+fn find_occupied_rooms(room_events: Vec<RoomEvent>, from: DateTime<Utc>, to: DateTime<Utc>) -> Vec<i32> {
+    let mut occupied_rooms: Vec<i32> = Vec::new();
+    for room_event in room_events {
+        // Event that has end date less than or equal to checkin date should not affect room
+        // availability
+        if room_event.end_date <= from || room_event.start_date >= to {
+            continue;
+        }
+
+        if room_event.end_date > from || room_event.end_date < to {
+            occupied_rooms.push(room_event.room_id);
+            continue;
+        }
+
+        if room_event.start_date > from || room_event.start_date < to {
+            occupied_rooms.push(room_event.room_id);
+            continue;
+        }
+    }
+
+    return occupied_rooms;
+
+}
+
 
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn find_available_rooms() {
+    fn find_busy_rooms() {
         // create rooms
         let mut rooms = Vec::new();
-        rooms.push(Room::new("1".to_string()));
-        rooms.push(Room::new("2".to_string()));
-        rooms.push(Room::new("3".to_string()));
+        rooms.push(Room::new(String::from("1")));
+        rooms.push(Room::new(String::from("2")));
+        rooms.push(Room::new(String::from("3")));
 
         assert_eq!(rooms.len(), 3);
 
@@ -77,25 +101,7 @@ mod test {
         let checkout_date = Utc.with_ymd_and_hms(2023, 12, 20, 0, 0, 0).unwrap();
 
         // Expect available rooms are [2,3], since room 1 has event 
-        let mut occupied_rooms: Vec<i32> = Vec::new();
-        for room_event in rooms_events {
-            // Event that has end date less than or equal to checkin date should not affect room
-            // availability
-            if room_event.end_date <= checkin_date || room_event.start_date >= checkout_date {
-                continue;
-            }
-
-            if room_event.end_date > checkin_date || room_event.end_date < checkout_date {
-                occupied_rooms.push(room_event.room_id);
-                continue;
-            }
-
-            if room_event.start_date > checkin_date || room_event.start_date < checkout_date {
-                occupied_rooms.push(room_event.room_id);
-                continue;
-            }
-
-        }
+        let mut occupied_rooms: Vec<i32> = find_occupied_rooms(rooms_events, checkin_date, checkout_date);
 
         assert_eq!(occupied_rooms, [1, 2]);
     }
