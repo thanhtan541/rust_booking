49bef6f3a8aa05fecba8230bac22d220b422ef0d
thanhtan541 <thanhtan004100@gmail.com>
2024-01-06 14:09:08 +0700

    feat: add criterion - benchmark tool

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,24 @@ fn internal_adder(a: i32, b: i32) -> i32 {
     a + b
 }
 
+#[inline]
+pub fn fibonacci(n: u64) -> u64 {
+    let mut a = 0;
+    let mut b = 1;
+
+    match n {
+        0 => b,
+        _ => {
+            for _ in 0..n {
+                let c = a + b;
+                a = b;
+                b = c;
+            }
+            b
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -22,3 +40,4 @@ mod tests {
         assert_eq!(4, internal_adder(2, 2));
     }
 }
+
